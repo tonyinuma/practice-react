390a0972618309ac2277450e71fc78dc838fc660
Tony ID <tony.inuma@gmail.com>
2021-10-14 11:52:09 -0500

    Add: test with arrays

---
+++ src/tests/07-deses-arr.test.js
@@ -0,0 +1,15 @@
+import { retornaArreglo } from "../base/07-deses-arr";
+
+describe("Pruebas en desestructuración", () => {
+    test("Debe retornar un string y un numero", () => {
+        const [ letras, numeros] = retornaArreglo();
+        // const arr = retornaArreglo();
+
+        // expect(arr).toEqual(['ABC', 123]);
+        expect(letras).toBe('ABC');
+        expect(typeof letras).toBe('string');
+        
+        expect(numeros).toBe(123);
+        expect(typeof numeros).toBe('number');
+    });
+});
