daa069852eb46a55c4284737d2b90dbb248e28d7
Tony ID <tony.inuma@gmail.com>
2021-10-14 09:34:15 -0500

    Add: Test function getSaludo

---
+++ src/tests/02-template-string.test.js
@@ -0,0 +1,19 @@
+import "@testing-library/jest-dom";
+import { getSaludo } from "../base/02-template-string";
+
+describe("Pruebas en 02-template-string.js", () => {
+  test("getSaludo debe retornar Hola José", () => {
+    const nombre = "José";
+
+    const saludo = getSaludo(nombre);
+
+    expect(saludo).toBe(`Hola ${nombre}`);
+  });
+
+  // getSaludo() debe retornar Hola Tony por defecto, Si no tiene argumento.
+  test("getSaludo debe retornar por defecto Hola Tony", () => {
+    const saludo = getSaludo();
+
+    expect(saludo).toBe(`Hola Tony`);
+  });
+});
