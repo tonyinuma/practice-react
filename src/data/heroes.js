bd0df43c92b8a165f4a3721c3c2c20ccfe8a7b7b
Tony ID <tony.inuma@gmail.com>
2021-10-14 12:01:07 -0500

    Fix: export object

--- src/data/heroes.js
+++ src/data/heroes.js
@@ -1,4 +1,4 @@
-export const heroes = [
+const heroes = [
     {
         id: 1,
         name: 'Batman',
@@ -25,3 +25,5 @@ export const heroes = [
         owner: 'Marvel'
     },
 ];
+
+export default heroes;
