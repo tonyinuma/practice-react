9cc0683d02f48a6953fa2a1915c3e4f1ac9972e2
Tony ID <tony.inuma@gmail.com>
2021-10-15 09:01:40 -0500

    Add: Pruebas con async-await fetch

---
+++ src/tests/11-async-await.test.js
@@ -0,0 +1,8 @@
+import { getImagen } from "../base/11-async-await";
+
+describe("Pruebas con async-await fetch", () => {
+    test("Debe retornar la url de una imagen", async () => {
+        const url = await getImagen();
+        expect(url.includes("https://")).toBe(true);
+    });
+});
