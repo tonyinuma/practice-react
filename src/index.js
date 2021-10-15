5f407b921d57ce72cd273039f294b34ca4c1927f
Tony ID <tony.inuma@gmail.com>
2021-10-15 09:24:18 -0500

    Update: index js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import PrimeraApp from './PrimeraApp';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import PrimeraApp from "./PrimeraApp";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <PrimeraApp saludo={'Hola Tony'} />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <PrimeraApp saludo={"Hola Tony"} />,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
