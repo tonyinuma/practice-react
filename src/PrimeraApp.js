af71e894882450b37ea438d2b6a9802663635412
Tony ID <tony.inuma@gmail.com>
2021-10-15 09:23:53 -0500

    Add: Primera App component

---
+++ src/PrimeraApp.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./index.css";
+
+const PrimeraApp = ({ saludo, subtitulo }) => {
+    return (
+        <>
+            <h1>{saludo}</h1>
+            <p>{subtitulo}</p>
+        </>
+    );
+};
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired,
+};
+
+PrimeraApp.defaultProps = {
+    subtitulo: "Soy un subtitulo",
+};
+
+export default PrimeraApp;
